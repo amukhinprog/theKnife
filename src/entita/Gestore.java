cd9afec5f88086d5870dc75a962c949a6c1f72b8
Nikoro02 <magrinnicolo455@gmail.com>
2025-05-05 17:12:53 +0200

    variabili, costruttore e primo metodo di aggiunta ristorante

--- src/entita/Gestore.java
+++ src/entita/Gestore.java
@@ -1,4 +1,6 @@
 package entita;
+import java.time.LocalDate;
+
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -10,5 +12,27 @@ package entita;
  * @author armuh
  */
 public class Gestore extends Utente{
-    int a;
+   private String nome;
+   private String cognome;
+   private String username;
+   private String password;
+   private LocalDate dataDiNascita;
+   private String luogodomicilio;
+   List<Ristorante> ristoranti = new ArrayList<>();
+
+   
+   
+ public Gestore (String nome, String cognome, String username, String password, LocalDate data,String luogodomicilio){
+     this.nome=nome;
+     this.cognome=cognome;
+     this.username=username;
+     this.password=password;
+     this.data=dataDiNascita;
+     this.luogodimicilio=luogodomicilio;
+ }  
+    void aggiungiRistorante(Ristorante r) {
+        ristoranti.add(r);
+    }
+}
+ 
 }
