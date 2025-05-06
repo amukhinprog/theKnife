4b28129e78e2317fac58693918f6a59709e10918
Nikoro02 <magrinnicolo455@gmail.com>
2025-05-06 22:07:18 +0200

    inizio classe iutente+file csv

--- src/entita/Utente.java
+++ src/entita/Utente.java
@@ -1,5 +1,9 @@
 package entita;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -10,5 +14,20 @@ package entita;
  * @author armuh
  */
 public class Utente {
-    
+     private String nome;
+   private String cognome;
+   private String username;
+   private String password;
+   private LocalDate dataNascita;
+   private String luogodomicilio;
+ 
+   public Utente(String nome, String cognome, String username, String password, LocalDate Data,String luogodomicilio){
+     this.nome=nome;
+     this.cognome=cognome;
+     this.username=username;
+     this.password=password;
+     this.dataNascita=Data;
+     this.luogodomicilio=luogodomicilio;
+ }  
+   
 }
