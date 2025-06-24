d980ca2185128dcf218528c0fa7a20cbb565a8f0
Nikoro02 <magrinnicolo455@gmail.com>
2025-06-25 00:45:08 +0200

    Merge branch 'main' of https://github.com/amukhinprog/theKnife
    # Conflicts:
    #	nbproject/private/private.properties

--- src/repository/GestoreService.java
+++ src/repository/GestoreService.java
@@ -17,11 +17,23 @@ import java.util.HashMap;
 public class GestoreService {
     private FileRecensioni fileRecensioni= new FileRecensioni();
     private HashMap<Integer, Recensione> recension = fileRecensioni.ottieniHashMap();
+    
    public HashMap<Integer, Recensione> getRecensioni() {
         return recension;
     }
 public void setRecensioni (HashMap<Integer, Recensione> recension) {
         this.recension = recension;
     }
+public double calcolaMediaStelle() {
+        if (recension == null || recension.isEmpty()) {
+            return 0.0;  // nessuna recensione
+        }
 
+        double somma = 0.0;
+        for (Recensione r : recension.values()) {
+            somma += r.getStelle();  // supponendo che getStelle() ritorni un int o double
+        }
+
+        return somma / recension.size();
+    }
 }
